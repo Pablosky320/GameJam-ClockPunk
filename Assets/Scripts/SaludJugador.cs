e27a23654e450d1c3d7cf911c1267c6139d00359
Eltipicalgorila <maxo010307@gmail.com>
2026-01-27 13:40:41 +0100

    a

--- Assets/Scripts/SaludJugador.cs
+++ Assets/Scripts/SaludJugador.cs
@@ -6,13 +6,14 @@ public class SaludJugador : MonoBehaviour
 {
     public float vidaMaxima = 100f;
     public float vidaActual;
-    public Slider sliderVida; 
+    public Slider sliderVida;
+    public GameObject escudo;
 
     void Start()
     {
         // Reiniciamos contador al empezar
-        SaludEnemigo.contadorMuertesGlobal = 0; 
-
+        SaludEnemigo.contadorMuertesGlobal = 0;
+        escudo.gameObject.SetActive(false);
         vidaActual = vidaMaxima;
         if (sliderVida != null) 
         {
@@ -23,14 +24,34 @@ public class SaludJugador : MonoBehaviour
 
     public void RecibirDanio(float cantidad)
     {
-        vidaActual -= cantidad;
-        vidaActual = Mathf.Clamp(vidaActual, 0, vidaMaxima);
-        
-        if (sliderVida != null) sliderVida.value = vidaActual;
+        if (escudo== false) {
+            {
+                vidaActual -= cantidad;
+                vidaActual = Mathf.Clamp(vidaActual, 0, vidaMaxima);
 
-        if (vidaActual <= 0)
+                if (sliderVida != null) sliderVida.value = vidaActual;
+
+                if (vidaActual <= 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        if (escudo == true)
+            {
+                Debug.Log("Daño bloqueado por el escudo");
+            }
+        }
+    }
+    //si tocamos el coleccionable, tendremos el escudo activado
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ColeccionableEscudo"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            escudo.gameObject.SetActive(true);
+        }
+        else
+        {
+            escudo.gameObject.SetActive(false);
         }
     }
 }
